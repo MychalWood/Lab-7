c769ffa5b232178ab47ade74a2bb7588308812c1
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-03-07 11:48:34 -0500

    Uploading the changes Aidan made on his end.

--- salaried_employee.class.php
+++ salaried_employee.class.php
@@ -1,27 +1,29 @@
 <?php
 /**
  * Author: Mychal Wood, Adam Hineiti, Aidan Tison
- * Date: 3/2/2023
+ * Date: 2/24/2023
  * File: salaried_employee.class.php
  * Description:
  */
 
-// requiring the employee.class.php page
-require_once 'employee.class.php';
-
 class SalariedEmployee extends Employee {
-    private $weekly_salary;
+
+    private float $weekly_salary;
 
     public function __construct($weekly_salary) {
         $this->weekly_salary = $weekly_salary;
     }
-    public function getWeeklySalary() {
+
+    public function getWeeklySalary(): float{
         return $this->weekly_salary;
     }
-    public function getPaymentAmount() {
+
+    public function getPaymentAmount(): float{
         return self::getPaymentAmount();
     }
-    public function __toString() {
-        return $this->getWeeklySalary() . ' ' . $this->getPaymentAmount();
+
+    public function toString() {
+        return $this->getWeeklySalary() . ', total amount: ' . $this->getPaymentAmount();
     }
 }
+?>
